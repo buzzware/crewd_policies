93f6ca6af31dddd80ccae0070b2f27cc022e0eab
Gary McGhee <gary@buzzware.com.au>
2016-08-23 16:10:46 +0800

    added support for jsonapi-resources gem

--- lib/crewd_policies.rb
+++ lib/crewd_policies.rb
@@ -2,6 +2,7 @@ require "crewd_policies/version"
 require "crewd_policies/model"
 require "crewd_policies/policy"
 require "crewd_policies/base_policy"
+require "crewd_policies/jsonapi_resources"
 
 module CrewdPolicies
   # Your code goes here...
